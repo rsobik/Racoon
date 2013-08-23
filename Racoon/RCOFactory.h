32c0b1d8e7e6e4fa380ace93ba7589a151760e8c
Raphael Sobik <raphael@sobik.org>
2013-08-23 21:50:16 +0200

    Adds more documentation and enables documentation warnings

--- Racoon/RCOFactory.h
+++ Racoon/RCOFactory.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-/***
+/**
  This is an abstract class that forms a basic factory that uses the
  provided assembly to construct the required components.
  
@@ -20,14 +20,20 @@
  */
 @interface RCOFactory : NSObject
 
-/***
+/**
+ Configures the factory to use the provided assembly to construct requested components.
+
  @param assembly The assembly that shall be used to constuct the requested components.
  **/
 - (void)configureWithAssembly:(id)assembly;
 
-/***
+/**
+ Requests the factory to provide a component of the requested type.
+ 
+ @warning Will raise an exception when an unkown type is requested.
+
  @param klass The type of the requested component.
- @return An object that is kind of klass.
+ @return The constructed object.
  **/
 - (id)componentForType:(Class)klass;
 
