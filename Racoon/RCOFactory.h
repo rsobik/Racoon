bc01bb3b7aa9afc12b10d86a9157c818cb60690a
Raphael Sobik <raphael@sobik.org>
2013-08-23 21:24:03 +0200

    Adds minimal documentation

--- Racoon/RCOFactory.h
+++ Racoon/RCOFactory.h
@@ -8,9 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+/***
+ This is an abstract class that forms a basic factory that uses the
+ provided assembly to construct the required components.
+ 
+ The components are constructed by calling a specific method on the
+ assembly. The method must have the following name: componentForType##TYPE##.
+
+ For example, when you call [SomeFactory componentForType:[FooBar class]],
+ the factory will call componentForTypeFooBar on the assembly.
+ */
 @interface RCOFactory : NSObject
 
+/***
+ @param assembly The assembly that shall be used to constuct the requested components.
+ **/
 - (void)configureWithAssembly:(id)assembly;
+
+/***
+ @param klass The type of the requested component.
+ @return An object that is kind of klass.
+ **/
 - (id)componentForType:(Class)klass;
 
 @end
